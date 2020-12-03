fdbd5e5b9b100b7344f526756f79c03dc8038410
Charlie Hastings <hasti123@users.noreply.github.com>
2020-12-03 10:26:56 -0600

    add day 3
    Added sample to debug algorithm

---
+++ 2020/3/day_3_part_1.py
@@ -0,0 +1,21 @@
+with open("data.txt") as f: hill = [list(i.strip()) for i in f.readlines()]
+
+current_row = 0
+current_column = 0
+
+max_column = len(hill[0])
+
+tree_count = 0
+
+while current_row < len(hill):
+	print(hill[current_row][current_column])
+	if hill[current_row][current_column] == '#':
+		tree_count += 1
+	current_row += 1
+	temp_column = current_column + 3
+	if temp_column >= max_column:
+		current_column = temp_column - max_column
+	else:
+		current_column = temp_column
+		
+print(tree_count)
---
+++ 2020/3/day_3_part_2.py
@@ -0,0 +1,31 @@
+def getTreeCount(hill, right, down):
+	current_row = 0
+	current_column = 0
+	
+	max_column = len(hill[0])
+	
+	tree_count = 0
+	
+	while current_row < len(hill):
+		print(hill[current_row][current_column])
+		if hill[current_row][current_column] == '#':
+			tree_count += 1
+		current_row += down
+		temp_column = current_column + right
+		if temp_column >= max_column:
+			current_column = temp_column - max_column
+		else:
+			current_column = temp_column
+			
+	return tree_count
+
+with open("data.txt") as f: hill = [list(i.strip()) for i in f.readlines()]
+
+slopes = [[1, 1], [3, 1], [5, 1], [7, 1], [1, 2]]
+
+tree_count_product = 1
+
+for slope in slopes:
+	tree_count_product *= getTreeCount(hill, slope[0], slope[1])
+	
+print(tree_count_product)
